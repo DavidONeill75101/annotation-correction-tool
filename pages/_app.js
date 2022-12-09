4cd844d23d1955561e9c5e3d13e51ad93aa29092
David O'Neill <2378524O@student.gla.ac.uk>
2022-12-09 13:50:43 +0000

    Updated favicon

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import { UserProvider } from '@auth0/nextjs-auth0';
 
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 
+import Head from "next/head";
+
 // https://medium.com/@fabianterh/fixing-flashing-huge-font-awesome-icons-on-a-gatsby-static-site-787e1cfb3a18
 // https://github.com/FortAwesome/react-fontawesome/issues/234
 import { config } from '@fortawesome/fontawesome-svg-core' // 👈
@@ -14,6 +16,9 @@ config.autoAddCss = false // 👈
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
+       <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </UserProvider>
   );
