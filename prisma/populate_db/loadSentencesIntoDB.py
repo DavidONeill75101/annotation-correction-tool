c8dc12729ae7683308b69772bbf0ff55928d63ad
David O'Neill <2378524O@student.gla.ac.uk>
2023-01-26 11:04:59 +0000

    db

--- prisma/populate_db/loadSentencesIntoDB.py
+++ prisma/populate_db/loadSentencesIntoDB.py
@@ -38,8 +38,7 @@ def main():
 
         row = sentences_pd.iloc[i]
         sql = "INSERT INTO sentences (matching_id, evidencetype, gene, cancer, drug, variant_group, day, formatted, journal, month, pmid, section, subsection, sentence, title, year) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        #val = (str(row['matching_id']), str(row['evidencetype']), str(row['gene_normalized']), str(row['cancer_normalized']), str(row['drug_normalized']), str(row['variant_id']), str(row['day']), str(row['formatted_sentence']),str(row['journal']),str(row['month']),str(row['pmid']),str(row['section']),str(row['subsection']), str(row['sentence']),str(row['title']),str(row['year']))
-        val = (str(row['matching_id']), str(row['evidencetype']), str(row['gene_normalized']), str(row['cancer_normalized']), str(row['drug_normalized']), str(row['variant_id']), str(row['day']), 'formatted_sentence',str(row['journal']),str(row['month']),str(row['pmid']),str(row['section']),str(row['subsection']), str(row['sentence']),str(row['title']),str(row['year']))
+        val = (str(row['matching_id']), str(row['evidencetype']), str(row['gene_normalized']), str(row['cancer_normalized']), str(row['drug_normalized']), str(row['variant_id']), str(row['day']), str(row['formatted_sentence']),str(row['journal']),str(row['month']),str(row['pmid']),str(row['section']),str(row['subsection']), str(row['sentence']),str(row['title']),str(row['year']))
         cur.execute(sql, val)
        
 
